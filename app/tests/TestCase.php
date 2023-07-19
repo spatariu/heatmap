1f40f23b57b9e2ece0ccf089c2d72c7cee1c914e
spatariu <spatariu.claudiu@yahoo.com>
2023-07-19 15:31:18 +0300

    Laravel 10 update

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -2,18 +2,9 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    
-    public function setUp(): void
-    {
-        parent::setUp();
-//        Artisan::call('passport:install');
-    }
-    
 }
